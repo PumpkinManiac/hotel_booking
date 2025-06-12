c077fd3fe0970e033d483b6745395154055744eb
PumpkinManiac <peeyush.verma10@gmail.com>
2025-06-12 16:50:19 +0530

    fifth commit

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,5 +1,5 @@
 import Hotel from "../models/Hotel.js";
-import Room from "../models/room.js";
+import Room from "../models/Room.js";
 import {v2 as cloudinary} from "cloudinary"
 
 //API to create a new room for a hotel
